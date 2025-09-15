ac059c6f95521596c133cbfbc38fe0ca5543c6f0
purvap45 <purvaphadol45@gmail.com>
2025-09-15 23:02:56 +0530

    front end validation done

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,49 +1,12 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-from family.models import FamilyMember, FamilyHead, State, City
+from django.db.models import Count, Q
+from family.models import FamilyMember, FamilyHead, State, City, statusChoice 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from family.forms import *
 from django.http import JsonResponse
-from family.forms import FamilyMemberForm, HobbyForm
-from django.db.models import Count
-from django.shortcuts import get_object_or_404
+from family.forms import FamilyMemberForm
 
-def update_family(request, pk):
-    head = get_object_or_404(FamilyHead, pk=pk)
-    HobbyFormSet = inlineformset_factory(FamilyHead, Hobby, form=HobbyForm, extra=0, can_delete=True)
-    MemberFormset = inlineformset_factory(FamilyHead, FamilyMember, form=FamilyMemberForm, extra=0, can_delete=True)
-
-    head_form = FamilyHeadForm(instance=head)
-    hobby_formset = HobbyFormSet(instance=head, prefix="hobbies")
-    member_formset = MemberFormset(instance=head, prefix="members")
-    if request.method == 'POST':
-        head_form = FamilyHeadForm(request.POST, request.FILES, instance=head)
-        hobby_formset = HobbyFormSet(request.POST, instance=head, prefix="hobbies")
-        member_formset = MemberFormset(request.POST, request.FILES, instance=head, prefix="members")
-
-        if head_form.is_valid() and hobby_formset.is_valid() and member_formset.is_valid():
-            head = head_form.save()
-            hobby_formset.save()
-            member_formset.save()
-            return JsonResponse({"success": True})
-        else:
-            return JsonResponse({
-                "success": False,
-                "head_errors": head_form.errors,
-                "hobby_errors": hobby_formset.errors,
-                "member_errors": member_formset.errors,
-            }, status=400)
-
-    context = {
-        'head_form': head_form,
-        'hobby_formset': hobby_formset,
-        'member_formset': member_formset,
-        'head': head
-    }
-    return render(request, 'update_family.html', context)
-
-
-# Create your views here.
 @login_required(login_url='login_page')
 def dashboard(request):
     heads = FamilyHead.objects.all()
@@ -58,27 +21,22 @@ def dashboard(request):
     }
     return render(request, 'dashboard.html', context)
 
+@login_required(login_url='login_page')
 def family_list(request):
-    heads = FamilyHead.objects.annotate(member_count=Count('members'))
+    heads = FamilyHead.objects.annotate(member_count= Count('members', filter=Q(status=1)))
     members = FamilyMember.objects.all()
     
     if request.GET.get('search'):
-        # print("hii")
         name = heads.filter(name__icontains=request.GET.get('search'))
         mobno = heads.filter(mobno__icontains=request.GET.get('search'))
-        # state = heads.filter(state__icontains=request.GET.get('search'))
-        # city = heads.filter(city__icontains=request.GET.get('search'))
-        
-        heads = name.union(mobno)
-        # print(heads)
-    # if heads.count() == 0:
-    #     messages.warning(request, "No result found.")
+        state = heads.filter(state__state_name__icontains=request.GET.get('search'))
+        city = heads.filter(city__city_name__icontains=request.GET.get('search'))
+        heads = name.union(mobno, state, city)
 
     p = Paginator(heads, 10)  
     page_number = request.GET.get('page')
     page_obj = p.get_page(page_number)
     totalPages = page_obj.paginator.num_pages
-
     context = {
         'members': members,
         'page_obj': page_obj,
@@ -87,10 +45,11 @@ def family_list(request):
     }
     return render(request, 'family_list.html', context)
 
+@login_required(login_url='login_page')
 def view_family(request, pk):
     head = FamilyHead.objects.get(id=pk)
-    members = FamilyMember.objects.filter(family_head_id=pk).all().exclude(status=statusChoice.DELETE)
-    hobbies = Hobby.objects.filter(family_head_id=pk).all().exclude(status=statusChoice.DELETE)
+    members = FamilyMember.objects.filter(family_head_id=pk).exclude(status=statusChoice.DELETE).all()
+    hobbies = Hobby.objects.filter(family_head_id=pk).exclude(status=statusChoice.DELETE).all()
     
     context = {
         'head': head,
@@ -99,6 +58,7 @@ def view_family(request, pk):
     }
     return render(request, 'view_family.html', context)
 
+@login_required(login_url='login_page')
 def update_head(request, pk):
     head = FamilyHead.objects.get(id=pk)
     head_form = FamilyHeadForm(instance=head)
@@ -119,6 +79,7 @@ def update_head(request, pk):
     }
     return render(request, 'update_head.html', context)
 
+@login_required(login_url='login_page')
 def add_hobby(request, pk):
     head = FamilyHead.objects.get(id=pk)
     HobbyFormSet = inlineformset_factory(FamilyHead, Hobby, form=HobbyForm, extra=1, formset=HobbyInlineFormSet)
@@ -140,6 +101,7 @@ def add_hobby(request, pk):
     }
     return render(request, 'add_hobby.html', context)
 
+@login_required(login_url='login_page')
 def update_hobby(request, pk):
     head = FamilyHead.objects.get(id=pk)
     HobbyFormSet = inlineformset_factory(FamilyHead, Hobby, form=HobbyForm, extra=0, formset=HobbyInlineFormSet)
@@ -147,6 +109,8 @@ def update_hobby(request, pk):
     if request.method == 'POST':
         hobby_formset = HobbyFormSet(request.POST, instance=head, prefix="hobbies")
         if hobby_formset.is_valid():
+            # for form in formset.deleted_forms:
+
             hobby_formset.save()
             return redirect('view_family', pk=pk)
     context = {
@@ -155,6 +119,7 @@ def update_hobby(request, pk):
     }
     return render(request, 'update_hobby.html', context)
 
+@login_required(login_url='login_page')
 def add_member(request, pk):
     head = FamilyHead.objects.get(id=pk)
     MemberFormset = inlineformset_factory(FamilyHead, FamilyMember, form=FamilyMemberForm, extra=1, formset=MemberInlineFormSet)
@@ -176,6 +141,7 @@ def add_member(request, pk):
     }
     return render(request, 'add_member.html', context)
 
+@login_required(login_url='login_page')
 def update_member(request, pk):
     head = FamilyHead.objects.get(id=pk)
     MemberFormset = inlineformset_factory(FamilyHead, FamilyMember, form=FamilyMemberForm, extra=0, formset=MemberInlineFormSet)
@@ -185,26 +151,80 @@ def update_member(request, pk):
         if member_formset.is_valid():
             member_formset.save()
             return redirect('view_family', pk=pk)
-        else:
-            print(member_formset.errors)
-            return JsonResponse({
-                "success": False,
-                "member_formset": member_formset.errors,
-            })
+        # else:
+        #     print(member_formset.errors)
+        #     return JsonResponse({
+        #         "success": False,
+        #         "member_formset": member_formset.errors,
+        #     })
     context = {
         'head': head,
         'member_formset': member_formset
     }
     return render(request, 'update_member.html', context)
 
+@login_required(login_url='login_page')
+def delete_family(request, pk):
+    head = FamilyHead.objects.get(id=pk)
+    if request.method == 'POST':
+        head.status = statusChoice.DELETE
+        head.save()
+        FamilyMember.objects.filter(family_head_id=head).update(status = statusChoice.DELETE)
+        return redirect('family_list')
+    context = {'head':head}
+    return render(request, 'delete_family.html', context)
+
+
+def update_family(request, pk):
+    head = FamilyHead.objects.get(id=pk)
+
+    HobbyFormSet = inlineformset_factory(
+        FamilyHead, Hobby, form=HobbyForm, extra=0, can_delete=True
+    )
+    MemberFormSet = inlineformset_factory(
+        FamilyHead, FamilyMember, form=FamilyMemberForm, extra=0, can_delete=True
+    )
+
+    head_form = FamilyHeadForm(instance=head)
+    hobby_formset = HobbyFormSet(instance=head, prefix="hobbies")
+    member_formset = MemberFormSet(instance=head, prefix="members")
+
+    if request.method == "POST":
+        head_form = FamilyHeadForm(request.POST, request.FILES, instance=head)
+        hobby_formset = HobbyFormSet(request.POST, instance=head, prefix="hobbies")
+        member_formset = MemberFormSet(request.POST, request.FILES, instance=head, prefix="members")
+
+        if head_form.is_valid() and hobby_formset.is_valid() and member_formset.is_valid():
+            head_form.save()
+            hobby_formset.save()
+            member_formset.save()
+            return redirect('view_family', pk=pk)
+            # return JsonResponse({"success": True})
+        else:
+            return JsonResponse({
+                "success": False,
+                "head_errors": head_form.errors,
+                "hobby_errors": hobby_formset.errors,
+                "member_errors": member_formset.errors,
+            }, status=400)
+
+    context = {
+        "head_form": head_form,
+        "hobby_formset": hobby_formset,
+        "member_formset": member_formset,
+        "head": head,
+    }
+    return render(request, "update_family.html", context)
+
+
 # def update_member(request, pk):
-#     head = FamilyHead.objects.get(id=pk)
-#     form = FamilyMemberForm(instance=head)
+#     member = FamilyMember.objects.get(id=pk)
+#     form = FamilyMemberForm(instance=member)
 #     if request.method == 'POST':
-#         form = FamilyMemberForm(request.POST, request.FILES, instance=head)
+#         form = FamilyMemberForm(request.POST, request.FILES, instance=member)
 #         if form.is_valid():
 #             form.save()
-#             return redirect('view_family', pk=pk)
+#             return redirect('view_family', pk=member.family_head.id)
 #         else:
 #             print(form.errors)
 #             return JsonResponse({
@@ -212,27 +232,7 @@ def update_member(request, pk):
 #                 "member_formset": form.errors,
 #             })
 #     context = {
-#         'head': head,
+#         'member': member,
 #         'form': form
 #     }
 #     return render(request, 'update_member.html', context)
-
-
-def delete_family(request, pk):
-    return render(request, 'delete_family.html')
-
-def update_single_member(request, pk):
-    member = FamilyMember.objects.get(id=pk)  # fetch single member
-    form = FamilyMemberForm(instance=member)
-
-    if request.method == "POST":
-        form = FamilyMemberForm(request.POST, request.FILES, instance=member)
-        if form.is_valid():
-            form.save()
-            return redirect("view_family", pk=member.family_head.id)
-
-    context = {
-        "form": form,
-        "member": member,
-    }
-    return render(request, "update_single_member.html", context)
--- static/js/formValidation.js
+++ static/js/formValidation.js
@@ -1,93 +1,108 @@
 const form = document.getElementById("familyForm");
+const name = document.getElementById("id_name");
+const surname = document.getElementById("id_surname");
+const dob = document.getElementById("id_dob");
+const mobno = document.getElementById("id_mobno");
+const address = document.getElementById("id_address");
+// const state = document.getElementById("id_state");
+const city = document.getElementById("id_city");
+const pincode = document.getElementById("id_pincode");
+// const marital_status = document.getElementById("id_marital_status")
+// console.log(marital_status)
+const photo = document.getElementById("id_photo");
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
-  //   const isValid = validateForm();
-  //   if (!isValid) return;
-  const formData = new FormData(form);
-  try {
-    const response = await fetch("/family_form/", {
-      method: "POST",
-      body: formData,
-    });
+  const isValid = validateForm();
+  // if (!isValid) return;
+  // const formData = new FormData(form);
+  // try {
+  //   const response = await fetch("/family_form/", {
+  //     method: "POST",
+  //     body: formData,
+  //   });
 
-    const result = await response.json();
-    console.log(result);
-    document.querySelectorAll('.errorMsg').forEach(span => span.innerText = '');
-    document.querySelectorAll('.errorInput').forEach(input => input.classList.remove('errorInput'));
+  //   const result = await response.json();
+  //   console.log(result);
+  //   document.querySelectorAll('.errorMsg').forEach(span => span.innerText = '');
+  //   document.querySelectorAll('.errorInput').forEach(input => input.classList.remove('errorInput'));
 
-    if (!result.success) {
-      // Head form errors
-      if (result.head_errors) {
-        for (const field in result.head_errors) {
-          const input = document.querySelector(`[name="${field}"]`);
-          if (input) {
-            setErrorMsg(input, result.head_errors[field][0]);
-          }
-        }
-      }
-      // Hobby formset errors
-      if (Array.isArray(result.hobby_errors)) {
-        result.hobby_errors.forEach((formErrors, i) => {
-          for (const field in formErrors) {
-            const input = document.querySelector(
-              `[name="hobbies-${i}-${field}"]`
-            );
-            if (input) {
-              setErrorMsg(input, formErrors[field][0]);
-            }
-          }
-        });
-      }
-      // Member formset errors
-      if (Array.isArray(result.member_errors)) {
-        result.member_errors.forEach((formErrors, i) => {
-          for (const field in formErrors) {
-            const input = document.querySelector(
-              `[name="members-${i}-${field}"]`
-            );
-            if (input) {
-              setErrorMsg(input, formErrors[field][0]);
-            }
-          }
-        });
-      }
-    } else if (result.success === true) {
-      console.log("true");
-      // alert(result.message);
-      window.location.href = "/";
-      // form.reset();
-    } else {
-      console.log("Something went wrong.", err);
-    }
-  } catch (err) {
-    console.log("Something went wrong.", err);
-  }
+  //   if (!result.success) {
+  //     // Head form errors
+  //     if (result.head_errors) {
+  //       for (const field in result.head_errors) {
+  //         const input = document.querySelector(`[name="${field}"]`);
+  //         if (input) {
+  //           setErrorMsg(input, result.head_errors[field][0]);
+  //         }
+  //       }
+  //     }
+  //     // Hobby formset errors
+  //     if (Array.isArray(result.hobby_errors)) {
+  //       result.hobby_errors.forEach((formErrors, i) => {
+  //         for (const field in formErrors) {
+  //           const input = document.querySelector(
+  //             `[name="hobbies-${i}-${field}"]`
+  //           );
+  //           if (input) {
+  //             setErrorMsg(input, formErrors[field][0]);
+  //           }
+  //         }
+  //       });
+  //     }
+  //     // Member formset errors
+  //     if (Array.isArray(result.member_errors)) {
+  //       result.member_errors.forEach((formErrors, i) => {
+  //         for (const field in formErrors) {
+  //           const input = document.querySelector(
+  //             `[name="members-${i}-${field}"]`
+  //           );
+  //           if (input) {
+  //             setErrorMsg(input, formErrors[field][0]);
+  //           }
+  //         }
+  //       });
+  //     }
+  //   } else if (result.success === true) {
+  //     console.log("true");
+  //     // alert(result.message);
+  //     window.location.href = "/";
+  //     // form.reset();
+  //   } else {
+  //     console.log("Something went wrong.", err);
+  //   }
+  // } catch (err) {
+  //   console.log("Something went wrong.", err);
+  // }
 });
 
 function setErrorMsg(input, errorMsg) {
   if (!input) return;
-    let inputField = input.closest("div");
-    // For radio buttons, 
-    if (input.type === "radio") {
-        while (inputField && !inputField.querySelector("span.errorMsg")) {
-            inputField = inputField.parentElement;
-        }
+  let inputField = input.closest("div");
+  // For radio buttons,
+  if (input.type === "radio") {
+    while (inputField && !inputField.querySelector("span.errorMsg")) {
+      inputField = inputField.parentElement;
     }
-    if (!inputField) return;
-    const span = inputField.querySelector("span.errorMsg");
-    if (!span) return;
-  
+  }
+  if (!inputField) return;
+  const span = inputField.querySelector("span.errorMsg");
+  if (!span) return;
+
   span.classList.add("errorMsg");
   input.classList.add("errorInput");
   span.innerText = errorMsg;
 }
 
-$(document).on("input", 'input[type="text"], input[type="date"], input[type="file"]', function () {
-  let span = $(this).parent().find(".errorMsg");
-  span.text("");
-  $(this).removeClass("errorInput");
-});
+$(document).on(
+  "input",
+  'input[type="text"], input[type="date"], input[type="file"], select, textarea',
+  function () {
+    let span = $(this).parent().find(".errorMsg");
+    span.text("");
+    $(this).removeClass("errorInput");
+  }
+);
 
 // $(document).on('change', 'input[type="radio"]', function() {
 //     // Find the container with the errorMsg span
@@ -96,3 +111,185 @@ $(document).on("input", 'input[type="text"], input[type="date"], input[type="fil
 //     span.text('');
 //     container.find('input[type="radio"]').removeClass('errorInput');
 // });
+
+const validateForm = () => {
+  const headValid = validateHead();
+  const hobbyValid = validateHobby();
+  const memberValid = validateMember();
+  return headValid && hobbyValid && memberValid;
+};
+
+const validateHead = () => {
+  let isValid = true;
+
+  const nameVal = name.value.trim();
+  if (nameVal === "") {
+    setErrorMsg(name, "Name is Required");
+    isValid = false;
+  } else if (nameVal.length < 3) {
+    setErrorMsg(name, "Name should have minimum 3 charaters");
+    isValid = false;
+  } else if (/\d/.test(nameVal)) {
+    setErrorMsg(name, "Name should not contain digits");
+    isValid = false;
+  }
+
+  const surnameVal = surname.value.trim();
+  if (surnameVal === "") {
+    setErrorMsg(surname, "Surname is Required");
+    isValid = false;
+  } else if (surnameVal.length < 3) {
+    setErrorMsg(surname, "Surname should have minimum 3 charaters");
+    isValid = false;
+  } else if (/\d/.test(surnameVal)) {
+    setErrorMsg(surname, "Surname should not contain digits");
+    isValid = false;
+  }
+
+  const dobVal = dob.value.trim();
+  const today = new Date();
+  const bDate = new Date(dobVal);
+  const age = today.getFullYear() - bDate.getFullYear();
+  const month = today.getMonth() - bDate.getMonth();
+  if (dobVal === "") {
+    setErrorMsg(dob, "Date of Birth is Required");
+    isValid = false;
+  } else if (age < 21 || (age === 10 && month < 0)) {
+    setErrorMsg(dob, "Age must be at least 21 years old.");
+    isValid = false;
+  }
+
+  const mobnoVal = mobno.value.trim();
+  if (mobnoVal === "") {
+    setErrorMsg(mobno, "Mobile No. is Required");
+    isValid = false;
+  } else if (!/^\d{10}$/.test(mobnoVal)) {
+    setErrorMsg(mobno, "Mobile No. should have 10 Digits.");
+    isValid = false;
+  }
+
+  const addressVal = address.value.trim();
+  if (addressVal === "") {
+    setErrorMsg(address, "Address is Required");
+    isValid = false;
+  }
+
+  const stateVal = state.value.trim();
+  if (!stateVal) {
+    setErrorMsg(state, "State is Required");
+  }
+
+  const cityVal = city.value.trim();
+  if (!cityVal) {
+    setErrorMsg(city, "City is Required");
+  }
+
+  const pincodeVal = pincode.value.trim();
+  if (pincodeVal === "") {
+    setErrorMsg(pincode, "Pincode is Required");
+  }
+
+  const maritalRadios = document.getElementsByName("marital_status");
+  let maritalChecked = false;
+
+  for (let i = 0; i < maritalRadios.length; i++) {
+    if (maritalRadios[i].checked) {
+      maritalChecked = true;
+      break;
+    }
+  }
+
+  if (!maritalChecked) {
+    // attach error to the first radio’s container
+    setErrorMsg(maritalRadios[0], "Please select Marital Status");
+    isValid = false;
+  }
+
+ // Wedding date validation if Married
+if (maritalChecked) {
+  let marriedVal = Array.from(maritalRadios).find(r => r.checked)?.value;
+  if (marriedVal && marriedVal.toLowerCase() === "married") {
+    const wedDateInput = document.querySelector('[name="wedding_date"]'); // update selector to match your form field
+    if (wedDateInput && !wedDateInput.value.trim()) {
+      setErrorMsg(wedDateInput, "Wedding Date is required if Married");
+      isValid = false;
+    }
+  }
+}
+
+
+  const photoPathVal = photo.value;
+  var allowedExtensions = /(\.jpg|\.png)$/i;
+  if (photoPathVal === "") {
+    setErrorMsg(photo, "Photo is Required");
+    isValid = false;
+  } else if (!allowedExtensions.exec(photoPathVal)) {
+    setErrorMsg(photo, "Invalid file type. Only PNG, JPG are allowed");
+    isValid = false;
+  } else {
+    var photoSize = photo.files[0].size / 1000 / 1000;
+    // console.log(photoSize);
+    if (photoSize > 2) {
+      setErrorMsg(photo, "Photo Size should be less than 2MB");
+      isValid = false;
+    }
+  }
+};
+
+function validateHobby() {
+  let isValid = true;
+  let firstHobby = $('#hobby-container .hobby-row:first input[type="text"]')[0];
+  $("#hobby-container .hobby-row:first .errorMsg").text("");
+  if (!firstHobby.value.trim()) {
+    setErrorMsg(firstHobby, "At least one hobby is required.");
+    isValid = false;
+  }
+  return isValid;
+}
+
+function validateMember() {
+  let isValid = true;
+
+  // Loop through each member row
+  $("#member-container .member-row").each(function () {
+    let row = $(this);
+
+    // Name
+    let m_name = row.find('input[type="text"][name$="member_name"]')[0];
+    if (!m_name.value.trim()) {
+      setErrorMsg(m_name, "Name is required.");
+      isValid = false;
+    }
+
+    // DOB
+    let m_dob = row.find('input[type="date"][name$="member_dob"]')[0];
+    if (!m_dob.value.trim()) {
+      setErrorMsg(m_dob, "Birth Date is required.");
+      isValid = false;
+    }
+
+    // Marital status
+    let maritalRadios = row.find('input[type="radio"][name$="member_marital"]');
+    let maritalChecked = maritalRadios.is(":checked");
+    if (!maritalChecked) {
+      setErrorMsg(maritalRadios[0], "Please select marital status.");
+      isValid = false;
+    }
+
+    // Wedding date required if Married
+    if (maritalChecked) {
+      let marriedVal = maritalRadios.filter(":checked").val();
+      if (marriedVal && marriedVal.toLowerCase() === "married") {
+        let wedDateInput = row.find(
+          'input[type="date"][name$="member_wedDate"]'
+        )[0];
+        if (!wedDateInput.value.trim()) {
+          setErrorMsg(wedDateInput, "Wedding date is required if married.");
+          isValid = false;
+        }
+      }
+    }
+  });
+
+  return isValid;
+}
